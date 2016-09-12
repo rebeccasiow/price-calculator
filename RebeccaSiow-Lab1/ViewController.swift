e5113f8794b5939242064c73a7789e15d1f04770
rebeccasiow <rebecca.siow@gmail.com>
2016-09-11 21:05:51 -0500

    Initial Commit

---
+++ RebeccaSiow-Lab1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RebeccaSiow-Lab1
+//
+//  Created by Rebecca Siow on 9/11/16.
+//  Copyright © 2016 Rebecca Siow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
